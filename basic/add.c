fd3ab9b5d5da8e6902074cb18a9d9778c7766107
yallabandimanoj <yallabandi.manojkumar>
2025-07-19 11:07:59 +0530

    add

---
+++ basic/add.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int add(int a,int b)
+{
+	int c =a+b;
+	printf("%d\n",c);
+
+}
+
+int main()
+{
+	int a;
+	int b;
+	add(100,200);
+}
---
+++ basic/format.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+	int a=10;
+	char ch ='a';
+	float f = 4.5;
+	double d = 7.500;
+	
+	printf("%d, %d %f %lf\n",a,ch,f,d);
+}
