eefd8a476f8a1b1262979a31ac54827983e7debb
yallabandimanoj <yallabandi.manojkumar>
2025-08-20 17:11:52 +0530

    strings

---
+++ strings/declare_str.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include<string.h>
+
+int main() {
+    
+    char str='c';
+
+   // scanf("%c",str);
+
+
+    //printf("The string is:%s %c\n",str, str[3]);
+    //printf("The string length is: %ld\n",strlen(str));
+    printf("%c",str);
+    return 0;
+}
