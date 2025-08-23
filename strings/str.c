d9c4a66447bfbf3ab1a21f4b8ca39b28d9f2da90
yallabandimanoj <yallabandi.manojkumar>
2025-08-23 17:49:42 +0530

    declare str

---
+++ strings/str.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+	char s[10] = {'h','e','l','l','o'};
+	int  n;
+//	n =sizeof(s)/sizeof(s[0]);
+	for(int i=0;s[i];i++)
+	printf("%d\n",s[i]);
+       //printf("\n");
+
+}
+
+
+
+
+
